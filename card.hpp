5165e6cc87d2851bd1ba0cf8a7c71f0cab4719c8
soyferfedor <soyferfedor@yandex.ru>
2025-06-26 19:20:04 +0300

    getters in myCards were added

--- card.hpp
+++ card.hpp
@@ -46,19 +46,27 @@ class myCards {
 	myCards(suit s1, rank r1, suit s2, rank r2) {card[0].setVal(s1, r1); card[1].setVal(s2, r2);}
 	myCards() {}
 	void setCards(suit s1, rank r1, suit s2, rank r2) {	card[0].setVal(std::make_pair(s1, r1));
-								card[1].setVal(std::make_pair(s2, r2));}
+														card[1].setVal(std::make_pair(s2, r2));}
 	void setCards(std::pair<suit, rank> in1, std::pair<suit, rank> in2) {	card[0].setVal(in1);
-										card[1].setVal(in2);}
+																			card[1].setVal(in2);}
 	void setCards(std::tuple<suit, rank, suit, rank> inTup) {	card[0].setVal(std::make_pair(std::get<0>(inTup), std::get<1>(inTup)));
-									card[1].setVal(std::make_pair(std::get<2>(inTup), std::get<3>(inTup)));}
+																card[1].setVal(std::make_pair(std::get<2>(inTup), std::get<3>(inTup)));}
 	void set1stCard(suit s, rank r) {card[0].setVal(std::make_pair(s, r));}
 	void set1stCard(std::pair<suit, rank> input) {card[0].setVal(input);}
 	void set2ndCard(suit s, rank r) {card[1].setVal(std::make_pair(s, r));}
 	void set2ndCard(std::pair<suit, rank> input) {card[1].setVal(input);}
-	//std::pair<suit, rank> getVal() const {return val;}
-	//написать различные геттеры 1-ой, 2-ой, обеих карт в разных форматах аналогично сеттерам
+	std::tuple<suit, rank, suit, rank> getVal() const {return std::tuple<suit, rank, suit, rank>
+																(card[0].getSuit(), card[0].getRank(), card[1].getSuit(), card[1].getRank());}
+	std::pair<suit, rank> get1stCard() const {return card[0].getVal();}
+	std::pair<suit, rank> get2ndCard() const {return card[1].getVal();}
+	suit getSuit1() const {return card[0].getSuit();}
+	suit getSuit2() const {return card[1].getSuit();}
+	rank getRank1() const {return card[0].getRank();}
+	rank getRank2() const {return card[1].getRank();}
 	~myCards() {}
 };
+
+
 
 
 
