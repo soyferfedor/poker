cf327eba5b90f2818109c64e20fcc0c82ddfecf0
soyferfedor <soyferfedor@yandex.ru>
2025-06-27 02:11:43 +0300

    functions to check flush and four of a kind were added

--- combinations.hpp
+++ combinations.hpp
@@ -6,11 +6,11 @@ enum class combinations {
 	TWO_PAIR,
 	THREE_OF_A_KIND,
 	STRAIGHT,
-	FLUSH,
+	FLUSH,																		// done
 	FULL_HOUSE,
-	FOUR_OF_A_KIND,
+	FOUR_OF_A_KIND,																// done
 	STRAIGHT_FLUSH,
-	ROYAL_FLUSH
+	ROYAL_FLUSH																	// done
 };
 
 Card maxRank (Card c1, Card c2) {
@@ -25,12 +25,10 @@ bool isEqualSuit (Card c1, Card c2) {
 	return c1.getSuit() == c2.getSuit();
 }
 
-bool isROYAL_FLUSH(std::vector<Card> vec) {
+bool isFLUSH(std::vector<Card> vec) {
 	if (vec.size() < 5)															// add check NO MORE THAN 7 CARDS
 		return 0;
 	int su[4] = {0,0,0,0};
-	suit s;
-	Card comb[5];
 	for (Card tmp : vec)
 		switch (tmp.getSuit()) {
 			case suit::SPADES		: su[0]++;
@@ -38,19 +36,60 @@ bool isROYAL_FLUSH(std::vector<Card> vec) {
 			case suit::DIAMONDS		: su[2]++;
 			case suit::CLUBS		: su[3]++;
 		}
-	if ((int)su[0] >= 4) {
+	return su[0]/5 + su[1]/5 + su[2]/5 + su[3]/5;
+}
+
+bool isFOUR_OF_A_KIND(std::vector<Card> vec) {
+	if (vec.size() < 4)															// add check NO MORE THAN 7 CARDS
+		return 0;
+	int su[13] = {0};
+	for (Card tmp : vec)
+		switch (tmp.getRank()) {
+			case rank::A		: su[0]++;
+			case rank::_2		: su[1]++;
+			case rank::_3		: su[2]++;
+			case rank::_4		: su[3]++;
+			case rank::_5		: su[4]++;
+			case rank::_6		: su[5]++;
+			case rank::_7		: su[6]++;
+			case rank::_8		: su[7]++;
+			case rank::_9		: su[8]++;
+			case rank::_10		: su[9]++;
+			case rank::J		: su[10]++;
+			case rank::Q		: su[11]++;
+			case rank::K		: su[12]++;
+		}
+	int sum = 0;
+	for (int t: su)
+		sum += t/4;
+	return sum;
+}
+
+bool isROYAL_FLUSH(std::vector<Card> vec) {
+	if (vec.size() < 5)															// add check NO MORE THAN 7 CARDS
+		return 0;
+	int su[4] = {0};
+	suit s;
+	for (Card tmp : vec)
+		switch (tmp.getSuit()) {
+			case suit::SPADES		: su[0]++;
+			case suit::HEARTS		: su[1]++;
+			case suit::DIAMONDS		: su[2]++;
+			case suit::CLUBS		: su[3]++;
+		}
+	if ((int)su[0] >= 5) {
 		s = suit::SPADES;
 		goto counting_ranks;
 	}
-	if ((int)su[1] >= 4) {
+	if ((int)su[1] >= 5) {
 		s = suit::HEARTS;
 		goto counting_ranks;
 	}
-	if ((int)su[2] >= 4) {
+	if ((int)su[2] >= 5) {
 		s = suit::DIAMONDS;
 		goto counting_ranks;
 	}
-	if ((int)su[3] >= 4) {
+	if ((int)su[3] >= 5) {
 		s = suit::CLUBS;
 		goto counting_ranks;
 	}
